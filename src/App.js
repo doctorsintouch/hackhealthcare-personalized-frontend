8dcac5c35e602959dd90c6db50b41856655c936e
nikodunk <n.dunkel@gmail.com>
2017-10-15 12:04:04 -0700

    interface done

--- src/App.js
+++ src/App.js
@@ -12,17 +12,37 @@ export default class App extends React.Component{
     super(props);
     this.state = {
             user: {
-              firstname: "niko",
-              lastname: 'dunk',
+              firstname: "",
+              lastname: "",
               dobday: '20',
               dobmonth: '01',
               dobyear: '1988',
               referrer: 'george',
               email: 'n.dunkel@gmail.com',
-              phone: '9177043031',
+              phone: '(917) 704 3031',
               },
-              loggedIn: false
-            }
+              loggedIn: false,
+              results: [
+                  {
+                    id: '9403930493f',
+                    type: 'Ophthalmologists',
+                    name: 'Dr. Pickle D. Rick', 
+                    address: '680 Haight Street', 
+                    phone: '(917) 704 3031',
+                    language: 'English, Spanish',
+                    rating: '*****'
+                  },
+                  {
+                    id: 'o2394kljl',
+                    type: 'Ophthalmologists',
+                    name: 'Dr. John A. Zoidberg', 
+                    address: '250 George Street', 
+                    phone: '(930) 450 375',
+                    language: 'English',
+                    rating: '****'
+                  }
+                ]
+        }
       
   }
 
@@ -37,44 +57,69 @@ export default class App extends React.Component{
           <div>
               { !this.state.loggedIn ?
             <div>
-              <p style={{color: "lightgrey", fontSize: "small"}}>WellBeing: A simple patient referral service</p>
-              { !this.state.firstname ? <h1>Hi! What is your name?</h1> : null}
-              { this.state.firstname ? <h1>Hi, {this.state.firstname}!</h1> : null}
-              <form>
-                first name<br />
-                <input
-                  autoFocus
-                  type="text"
-                  onChange={this.handleUsernameChange('firstname').bind(this)}
-                  value={this.state.firstname}
-                /><br /><br />
-                last name<br />
-                <input
-                  type="text"
-                  value={this.state.lastname}
-                /><br /><br />
-                birthday<br />
-                <input
-                  type="date"
-                  value={this.state.dobyear}
-                /><br /><br />
-                email<br />
-                <input
-                  type="email"
-                  value={this.state.email}
-                />
-                <input
-                  className="submitButton"
-                  type="submit"
-                  class="btn btn-success"
-                  value="Let's Go!"
-                  onClick={this.handleUsernameSubmit.bind(this)} />
-              </form>
+              <p style={{color: "lightgrey", fontSize: "small"}}>Doctors In Touch: Connecting you to personalized providers</p>
+              <h1>Good afternoon, Dr. Lim!</h1>
+              <div class="flex">
+                <div style={{maxWidth: 300, marginTop: 50}}>
+                  <form id="form1">
+                    Patient First Name<br />
+                    <input
+                      autoFocus
+                      type="text"
+                      class="form-control"
+                      onChange={this.handleUsernameChange('firstname').bind(this)}
+                      value={this.state.firstname}
+                    /><br /><br />
+                    Patient Last Name<br />
+                    <input
+                      class="form-control"
+                      type="text"
+                      value={this.state.lastname}
+                    /><br /><br />
+                    Date of Birth<br />
+                    <input
+                      type="date"
+                      class="form-control"
+                      value={this.state.dobyear}
+                    /><br /><br />
+                    Email<br />
+                    <input
+                      type="email"
+                      class="form-control"
+                      value={this.state.email}
+                    /> <br /><br />
+                    <button 
+                    type="submit" 
+                    form="form1" 
+                    value="Submit"
+                    style={{width: 300}}
+                    className="submitButton"
+                    type="submit"
+                    class="btn btn-lg btn-success"
+                    onClick={this.handleUsernameSubmit.bind(this)}>
+                      <b>Refer {this.state.user.firstname} {this.state.user.lastname}</b>
+                    </button>
+                  </form>
+                </div>
+                <div class="border" style={{width: 100, marginTop: 50}}></div>
+                <div style={{maxWidth: 300, marginTop: 50}}>
+                  <form>
+                    Patient ID<br />
+                    <input
+                      type="text"
+                      class="form-control"
+                    /><br /><br />
+                  </form>
+                </div>
+              </div>
             </div>
             : null
           }
               { this.state.loggedIn ?
-                <Results />
+                <Results 
+                results={this.state.results}
+                patient={this.state.user}
+                receivedData={this.receivedData} />
                 : null
               }
             </div>
@@ -83,14 +128,13 @@ export default class App extends React.Component{
 
     handleUsernameChange () {
       return function (e) {
-        var state = {};
-        state.firstname = e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1);
-        this.setState({firstname: state.firstname});
+        var user = {};
+        user.firstname = e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1);
+        this.setState({
+            user: user
+          })
       }.bind(this);
-
     }
-
-
 
 
     handleUsernameSubmit(e) {
@@ -114,10 +158,12 @@ export default class App extends React.Component{
         this.setState({
             loggedIn: true
           })
-        
       }
 
 
+      receivedData(data){
+        console.log(data)
+      }
 
 }
 
@@ -126,39 +172,22 @@ export default class App extends React.Component{
 
 class Results extends React.Component{
 
-  results = [
-        {
-          id: '9403930493f',
-          type: 'Nephrologist',
-          name: 'Dr. Nice Person', 
-          address: '680 Haight Street', 
-          phone: '9177043031'
-        },
-        {
-          id: 'o2394kljl',
-          type: 'Onconephrologist',
-          name: 'Dr. Steven George', 
-          address: '250 George Street', 
-          phone: '930450375'
-        }
-        ]
-
   componentDidMount() {
 
     $.ajax({
       type: "GET",
-      url: 'https://localhost:3000/results&id=3',
+      url: 'http://www.hackhealthcare-personalized.info:3000/results&id=3',
       dataType: 'json',
       cache: false,
       success: function(data) {
-        this.results = data
+        this.props.receivedData(data)
       }.bind(this),
       error: function(xhr, status, err) {
         console.error(this.props.url, status, err.toString());
       }.bind(this)
     });
 
-    console.log(this.results)
+    this.props.receivedData('onetwothree')
   }
 
 
@@ -167,31 +196,48 @@ class Results extends React.Component{
 
     return (
       <div>
-      <h1>Nephrologists in your area</h1>
-      <button style={{display: 'inline'}} class="btn btn-default"><span class="glyphicon glyphicon-remove"></span> Location: within 1 mile of your home</button> &nbsp;
-      <button style={{display: 'inline'}} class="btn btn-default"><span class="glyphicon glyphicon-remove"></span> Insurance: covered by Aetna</button>
+      <div class="flex">
+        <div>
+          <h1>Ophthalmologists in {this.props.patient.firstname}s area </h1>
+          <button style={{display: 'inline'}} class="btn btn-default"><span class="glyphicon glyphicon-remove"></span> Location: within 1 mile of {this.props.patient.firstname}s home</button> &nbsp;
+          <button style={{display: 'inline'}} class="btn btn-default"><span class="glyphicon glyphicon-remove"></span> Insurance: covered by Aetna</button>
+        </div>
+      </div>
         <div style={{paddingTop: 50}}>
-          {this.results.map(result => (
-            <div key={result.id} style={{maxWidth: 900}}>
-              <div style={{float: 'right'}}>
-                <iframe style={{border:0}} src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBz8soRKrBNMALn5zTxtH2grSVPbi2nSK4 
-                  &q=Space+Needle,Seattle+WA" allowfullscreen>
-                </iframe>
-              </div>
-              <div >
-                <h4>{result.name}</h4>
-                <p>{result.type}</p>
-                <p>{result.address}</p>
-                <p>Languages: English</p>
-                <p>{result.phone}</p><br />
-                <button class="btn btn-success"><span class="glyphicon glyphicon-check"></span> Get referral</button> &nbsp;
-                <button class="btn btn-secondary"><span class="glyphicon glyphicon-star"></span></button>
+          {this.props.results.map(result => (
+            <div key={result.id}>
+              <div class="flex">
+                <div class="nugget">
+                  <h4>{result.name}</h4>
+                  <p>{result.type}</p>
+                  <p>{result.address}</p>
+                  <p>{result.language}</p>
+                  <p>{result.phone}</p><br />
+                  <button class="btn btn-success"><span class="glyphicon glyphicon-check"></span> Make Referral</button> &nbsp;
+                  <button class="btn btn-default"><span class="glyphicon glyphicon-menu-right"></span>Schedule now</button> &nbsp;
+                </div>
+                <div class="nugget">
+                  <iframe style={{border:0}} src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBz8soRKrBNMALn5zTxtH2grSVPbi2nSK4 
+                    &q=Space+Needle,Seattle+WA" allowFullScreen>
+                  </iframe><br /><br />
+                  <button style={{backgroundColor: 'pink'}} class="btn btn-secondary"><span class="glyphicon glyphicon-heart"></span></button>
+                </div>
+                <div class="nugget">
+                  <b>Avg. Rating</b>
+                  <h5><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span></h5>
+                  <br /><br />
+                  <b>Reviews</b>
+                  <p>N/A</p>
+                </div>
               </div>
               <hr />
-            </div>
-
-
-          ))}
+              </div> ))}
+          <div style={{marginLeft: 50}}>
+          <p style={{textAlign: 'right', paddingTop:50}}><button class="btn btn-lg btn-info">
+            <span class="glyphicon glyphicon-share"></span> Share list with {this.props.patient.firstname}
+          </button>
+          </p>
+          </div>
         </div>
         
       </div>
